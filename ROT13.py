2d3010cf6a2cb2375ccbd5d865441f8a7d68dad9
AlkisTag <36636915+AlkisTag@users.noreply.github.com>
2018-02-22 21:41:47 +0200

    Update ROT13.py

--- ROT13.py
+++ ROT13.py
@@ -1,6 +1,6 @@
 import codecs
 word = input("Gimme a word or a phrase !ONLY ENGLISH!\n")
-turn_it_arround = codecs.getencoder( "rot-13" )
+turn_it_arround = codecs.getencoder( "rot-13" ) #Elpizo na epitrepetai autos o tropos kathos den exei shmeiothei oti apagoreyetai h xrhsh toy
 wordP  = turn_it_arround( word )[0]
 print ("This is the result:")
 print (wordP)
